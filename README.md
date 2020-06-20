02f64687858f19deffd6250c2e2b6ba4fee1c836
semioys <semioys@gmail.com>
2020-06-20 15:29:36 +0300

    TypeScript was added to project

---
+++ README.md
@@ -0,0 +1,8 @@
+# How to start project
+
+```sh
+$ npm install
+$ pm2 install -g
+$ pm2 install typescript
+$ npm run dev
+```
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
-const express = require('express');
+import express from 'express'
 const app = express();
-const PORT = 3000;
+const { PORT } = process.env;
 
 app.get('/', (req, res) => {
   res.send('Hello world!!')

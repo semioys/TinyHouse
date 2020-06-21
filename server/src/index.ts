4c23e5b3a1203e714ec0e6003553bb786e7d40d9
semioys <semioys@gmail.com>
2020-06-21 23:50:28 +0300

    GraphQL API implementation was changed with Apollo server features

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
-import { schema } from "./graphql";
+import { typeDefs, resolvers } from "./graphql";
 
 const app = express();
 const { PORT } = process.env;
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.applyMiddleware({ app, path: "/api" });
 
